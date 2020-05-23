b8719c07c84bf14c04133a8660f7ad72c2dcd765
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-05-23 23:27:02 +0530

    htds

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 a=[x for x in range(20)]
+b=''
+for i in a:
+    b+=str(i)
 @app.route('/')
 def index():
-    return '<h1>Hello There How</h1>'
+    return '<h1>Hello There How</h1>'+b
 
 if __name__ == '__main__':
     app.run(debug=True)
