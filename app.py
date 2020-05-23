c7f215fd62bd9e29943890f6db12fae779b30165
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-05-23 22:30:53 +0530

    ewsfr

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 a=[x for x in range(50)]
 def index():
-    return str(a)
+    return '<h1>Hello There</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
