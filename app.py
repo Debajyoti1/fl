df28eb71ff9a332fe99582152b2151dd0f2d8b7a
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2020-05-23 22:24:40 +0530

    ss

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
+a=[x for x in range(5000000)]
 def index():
-    return 'hello world 56gsdg'
+    return a
 
 if __name__ == '__main__':
     app.run(debug=True)
